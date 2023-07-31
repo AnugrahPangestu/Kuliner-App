75b9924cc5bb4d55be6d01ddd11900b2619c97d9
MuhammadAnugrahPangestu <75356267+MuhammadAnugrahPangestu@users.noreply.github.com>
2023-07-31 17:03:06 +0700

    Final Project

--- app/src/main/java/com/example/kulinerjepang/AboutProfile.kt
+++ app/src/main/java/com/example/kulinerjepang/AboutProfile.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class AboutProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_about_profile)
     }
 }
